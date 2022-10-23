a99570b42af09559b16b7f2c61bac92d2f8ee343
yuki1303 <baskeinmss@gmail.com>
2022-10-23 18:51:47 +0900

    create LoginScreen and SignUpScreen

--- App.jsx
+++ App.jsx
@@ -1,14 +1,18 @@
 // import React from 'react';
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import LogInScreen from './src/screens/LogInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
   return (
     // <MemoListScreen />
     // <MemoDetailScreen />
-    <MemoEditScreen />
+    // <MemoEditScreen />
     // <MemoCreateScreen />
+    // <LogInScreen />
+    <SignUpScreen />
   );
 }
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,35 +1,34 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { string, bool, shape } from 'prop-types';
+// import React from 'react';
+// import { View, Text, StyleSheet } from 'react-native';
+// import { string, bool, shape } from 'prop-types';
 
-function Hello(props) {
-  const { children, bang, style } = props;
-  return (
-    <View>
-      
-    </View>
-  );
-}
+// function Hello(props) {
+//   const { children, bang, style } = props;
+//   return (
+//     <View>
+//     </View>
+//   );
+// }
 
-Hello.propTypes = {
-  children: string.isRequired,
-  bang: bool,
-  style: shape(),
-};
+// Hello.propTypes = {
+//   children: string.isRequired,
+//   bang: bool,
+//   style: shape(),
+// };
 
-Hello.defaultProps = {
-  bang: false,
-  style: null,
-};
+// Hello.defaultProps = {
+//   bang: false,
+//   style: null,
+// };
 
-const styles = StyleSheet.create({
-  text: {
-    color: '#ffffff',
-    backgroundColor: 'blue',
-    fontSize: 40,
-    fontWeight: 'bold',
-    padding: 16,
-  },
-});
+// const styles = StyleSheet.create({
+//   text: {
+//     color: '#ffffff',
+//     backgroundColor: 'blue',
+//     fontSize: 40,
+//     fontWeight: 'bold',
+//     padding: 16,
+//   },
+// });
 
-export default Hello;
+// export default Hello;
