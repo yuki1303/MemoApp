2655c16f271c32472faa6ce0517268c02c6e535b
yuki1303 <baskeinmss@gmail.com>
2022-10-23 14:01:26 +0900

    Isstall @expo/vector-icons

--- App.jsx
+++ App.jsx
@@ -1,10 +1,10 @@
 // import React from 'react';
-// import MemoListScreen from './src/screens/MemoListScreen';
-import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
-    // <MemoListScreen />
-    <MemoDetailScreen />
+    <MemoListScreen />
+    // <MemoDetailScreen />
   );
 }
